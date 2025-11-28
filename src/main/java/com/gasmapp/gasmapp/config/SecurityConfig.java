8590a37d9e42d0a61a98d2ab0139bddf1280ba0f
lucas-bergami <lucasbergami5@gmail.com>
2025-11-28 08:01:53 -0300

    Revert "fix bugs 2"
    This reverts commit e188cff60ff29665e54bad230bcb2d0d6f30c324.

--- src/main/java/com/gasmapp/gasmapp/config/SecurityConfig.java
+++ src/main/java/com/gasmapp/gasmapp/config/SecurityConfig.java
@@ -2,19 +2,38 @@ package com.gasmapp.gasmapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()          // desativa proteção CSRF, útil para APIs REST
+                .csrf().disable()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll(); // permite que qualquer usuário acesse todos os endpoints
+                .requestMatchers("/clients/**").hasRole("USER") // exige role USER para esse endpoint
+                .anyRequest().authenticated() // outros endpoints requerem autenticação
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
 
         return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_"); // Keycloak já envia roles como "USER", "ADMIN", etc.
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("realm_access"); // Claim onde o Keycloak armazena roles
+
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return converter;
     }
 }
